dc762f50d07493a9fdca9d645ecc2374c455b0c2
danju4rizzl <danjumashiwaju@gmail.com>
2024-02-07 11:42:30 +0200

    code refactored with routes

--- src/main.ts
+++ src/main.ts
@@ -1,64 +1,19 @@
-import { PlaywrightCrawler, Dataset } from 'crawlee'
+import { PlaywrightCrawler, log } from 'crawlee'
+import { router } from './route.js'
 
+// This is better set with CRAWLEE_LOG_LEVEL env var
+// or a configuration option. This is just for show 😈
+log.setLevel(log.LEVELS.DEBUG)
+
+log.debug('Setting up crawler.')
 const crawler = new PlaywrightCrawler({
-  requestHandler: async ({ page, enqueueLinks, request }) => {
-    // console.log(request.url)
-    if (request.label === 'DETAIL') {
-      // what to do when we get to a product detail page
+  // I'm using a low maxRequestsPerCrawl so the
+  // crawler ends quicker for demo purposes
+  maxRequestsPerCrawl: 50,
 
-      const title = await page.locator('.product-meta h1').textContent()
-      const sku = await page.locator('.product-meta__sku-number').textContent()
-
-      const priceElement = page
-        .locator('span.price')
-        .filter({
-          hasText: '$'
-        })
-        .first()
-
-      const currentPrice = await priceElement.textContent()
-      const rawPrice = currentPrice?.split('$')[1]
-      const price = Number(rawPrice?.replace(',', ''))
-
-      const inStockElement = page
-        .locator('span.product-form__inventory')
-        .filter({
-          hasText: 'In Stock'
-        })
-        .first()
-
-      const inStock = (await inStockElement.count()) > 0
-
-      await Dataset.pushData({ title, sku, price, inStock })
-      await Dataset.exportToJSON('saved-products')
-      await Dataset.exportToCSV('saved-products')
-    } else if (request.label === 'COLLECTION') {
-      // what to do when we get to a collection page
-      const productSelector = '.product-item > a'
-      const nextPageSelector = 'a.pagination__next'
-
-      await page.waitForSelector(productSelector)
-      await enqueueLinks({
-        selector: productSelector,
-        label: 'DETAIL'
-      })
-
-      const nextButton = await page.$(nextPageSelector)
-      if (nextButton) {
-        await enqueueLinks({
-          selector: nextPageSelector,
-          label: 'COLLECTION'
-        })
-      }
-    } else {
-      const collectionSelector = '.collection-block-item'
-      await page.waitForSelector(collectionSelector)
-      await enqueueLinks({
-        selector: collectionSelector,
-        label: 'COLLECTION'
-      })
-    }
-  }
+  // Instead of the long requestHandler with
+  // if clauses we provide a router instance.
+  requestHandler: router
 })
 
 await crawler.run(['https://warehouse-theme-metal.myshopify.com/collections'])
